af53ab2b219488b79256d8595cb2ad3cc6c5cf23
Dona-aa <edonaperaj4@gmail.com>
2026-09-15 10:56:37 +0200

    feat: add database connection

---
+++ src/lib/server/database.js
@@ -0,0 +1,15 @@
+
+import mysql from 'mysql2/promise';
+import { env } from '$env/dynamic/private';
+ 
+// Verbindungspool zur MySQL-Datenbank
+export const db = mysql.createPool({
+	host: env.DB_HOST,
+	port: Number(env.DB_PORT),
+	user: env.DB_USER,
+	password: env.DB_PASSWORD,
+	database: env.DB_NAME,
+ 
+	waitForConnections: true,
+	connectionLimit: 5
+});
